5a178afab1009be9cc4d6a5ba743ca23208a4f0e
fuh-red <Frederick.Peralta01@student.csulb.edu>
2024-05-02 21:57:31 -0700

    changed session duration

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
     store: MongoStore.create({ mongoUrl: uri}),
-    cookie: { maxAge: 1000 * 30 } //30 second session
+    cookie: { maxAge: 1000 * 60 * 60 } //1 hour duration
 }));
 
 
